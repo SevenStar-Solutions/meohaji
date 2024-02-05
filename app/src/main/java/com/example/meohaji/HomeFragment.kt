b049e8ba6390640d47889ccbfde37d3160164db8
phs8505 <phs8505@gmail.com>
2024-02-05 20:24:54 +0900

    [add & feat] : Home 프래그먼트 관련 파일 추가, 리사이클러뷰에 아이템 노출
    1. 리사이클러뷰 필요한 어댑터 파일과 데이터 클래스 파일
    2. 테스트용으로 메인 액티비티에 Home 프래그먼트 뜨도록 설정

---
+++ app/src/main/java/com/example/meohaji/HomeFragment.kt
@@ -0,0 +1,132 @@
+package com.example.meohaji
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.meohaji.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    private val mostPopularVideoAdapter by lazy {
+        MostPopularVideoAdapter(requireContext())
+    }
+
+    private val categoryList = listOf(
+        "Film & Animation",
+        "Autos & Vehicles",
+        "Music",
+        "Pets & Animals",
+        "Sports",
+        "Short Movies",
+        "Travel & Events",
+        "Gaming",
+        "Videoblogging",
+        "People & Blogs",
+        "Comedy",
+        "Entertainment",
+        "News & Politics",
+        "Howto & Style",
+        "Education",
+        "Science & Technology",
+        "Nonprofits & Activism",
+        "Movies",
+        "Anime/Animation",
+        "Action/Adventure",
+        "Classics",
+        "Comedy",
+        "Documentary",
+        "Drama",
+        "Family",
+        "Foreign",
+        "Horror",
+        "Sci-Fi/Fantasy",
+        "Thriller",
+        "Shorts",
+        "Shows",
+        "Trailers"
+    )
+
+    private val sortList = listOf(
+        "추천 순",
+        "조회수 순",
+        "좋아요 순",
+        "최신 순"
+    )
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.rvHomeMostPopularVideo.adapter = mostPopularVideoAdapter
+        mostPopularVideoAdapter.submitList(dummy)
+
+        val adapter1 = ArrayAdapter(
+            requireContext(),
+            com.bumptech.glide.R.layout.support_simple_spinner_dropdown_item,
+            categoryList
+        )
+        binding.spinnerHomeCategory.adapter = adapter1
+
+        val adapter2 = ArrayAdapter(
+            requireContext(),
+            com.bumptech.glide.R.layout.support_simple_spinner_dropdown_item,
+            sortList
+        )
+        binding.spinnerHomeSortVideo.adapter = adapter2
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+
+    private val dummy = listOf<MostPopularVideo>(
+        MostPopularVideo(
+            id = "kW_z-NMuZIU",
+            publishedAt = "2024-02-02T21:30:04Z",
+            channelTitle = "쿠팡플레이 스포츠",
+            title = "[2023 AFC 카타르 아시안컵] 2023 AFC 카타르 아시안컵 호주 VS 대한민국 풀 하이라이트",
+            description = "쿠팡플레이에서 2023 AFC 카타르 아시안컵 생중계와 풀 하이라이트",
+            thumbnail = "https://i.ytimg.com/vi/kW_z-NMuZIU/mqdefault.jpg"
+        ),
+        MostPopularVideo(
+            id = "kW_z-NMuZIU",
+            publishedAt = "2024-02-02T21:30:04Z",
+            channelTitle = "쿠팡플레이 스포츠",
+            title = "[2023 AFC 카타르 아시안컵] 2023 AFC 카타르 아시안컵 호주 VS 대한민국 풀 하이라이트",
+            description = "쿠팡플레이에서 2023 AFC 카타르 아시안컵 생중계와 풀 하이라이트",
+            thumbnail = "https://i.ytimg.com/vi/kW_z-NMuZIU/mqdefault.jpg"
+        ),
+        MostPopularVideo(
+            id = "kW_z-NMuZIU",
+            publishedAt = "2024-02-02T21:30:04Z",
+            channelTitle = "쿠팡플레이 스포츠",
+            title = "[2023 AFC 카타르 아시안컵] 2023 AFC 카타르 아시안컵 호주 VS 대한민국 풀 하이라이트",
+            description = "쿠팡플레이에서 2023 AFC 카타르 아시안컵 생중계와 풀 하이라이트",
+            thumbnail = "https://i.ytimg.com/vi/kW_z-NMuZIU/mqdefault.jpg"
+        ),
+        MostPopularVideo(
+            id = "kW_z-NMuZIU",
+            publishedAt = "2024-02-02T21:30:04Z",
+            channelTitle = "쿠팡플레이 스포츠",
+            title = "[2023 AFC 카타르 아시안컵] 2023 AFC 카타르 아시안컵 호주 VS 대한민국 풀 하이라이트",
+            description = "쿠팡플레이에서 2023 AFC 카타르 아시안컵 생중계와 풀 하이라이트",
+            thumbnail = "https://i.ytimg.com/vi/kW_z-NMuZIU/mqdefault.jpg"
+        )
+    )
+}
--- app/src/main/java/com/example/meohaji/MainActivity.kt
+++ app/src/main/java/com/example/meohaji/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.meohaji
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import com.example.meohaji.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbarMain)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        setFragment(HomeFragment())
+    }
+
+    private fun setFragment(fragment: Fragment) {
+        supportFragmentManager.commit {
+            replace(R.id.frame_main, fragment)
+            setReorderingAllowed(true)
+            addToBackStack(null)
+        }
     }
 }
