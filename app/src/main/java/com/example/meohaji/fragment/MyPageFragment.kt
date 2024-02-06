e32425bfb593ad9230a2eb3d71c3dea9aa7debb7
Box-o <jeffmailkr@gmail.com>
2024-02-06 10:44:09 +0900

    feat : 다이얼로그 버튼에 실행 추가 / layout : 다이얼로그 레이아웃 작성 / style : coner_radius 모서리 곡률

--- app/src/main/java/com/example/meohaji/fragment/MyPageFragment.kt
+++ app/src/main/java/com/example/meohaji/fragment/MyPageFragment.kt
@@ -1,5 +1,6 @@
 package com.example.meohaji.fragment
 
+import android.app.AlertDialog
 import android.app.Dialog
 import android.graphics.Color
 import android.os.Bundle
@@ -19,6 +20,7 @@ import androidx.core.content.ContextCompat
 import com.example.meohaji.R
 import com.example.meohaji.databinding.FragmentMyPageBinding
 import com.example.meohaji.databinding.FragmentSearchBinding
+import de.hdodenhof.circleimageview.CircleImageView
 import java.nio.file.Files.find
 
 class MyPageFragment : Fragment() {
@@ -47,7 +49,48 @@ class MyPageFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.btnEditName.setOnClickListener {
-//            val dialog = Dialog(context).apply { setContentView(R.layout) }
+            val dialogView = LayoutInflater.from(requireContext()).inflate(R.layout.dialog_mydata, null)
+            val profileImg = dialogView.findViewById<CircleImageView>(R.id.civ_dialog_profile)
+
+//            dialogView.background =
+            val builder = AlertDialog.Builder(requireContext())
+            builder.setView(dialogView)
+            builder.setTitle("커스텀 다이얼로그")
+            builder.setIcon(R.mipmap.ic_launcher)
+            builder.setPositiveButton("확인") { dialog, which ->
+                // 확인 버튼을 클릭했을 때의 동작
+            }
+            builder.setNegativeButton("취소") { dialog, which ->
+                // 취소 버튼을 클릭했을 때의 동작
+            }
+
+            val dialog = builder.create()
+            dialog.show()
+
+
+            /*
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("커스텀 다이얼로그")
+            builder.setIcon(R.mipmap.ic_launcher)
+
+            val v1 = layoutInflater.inflate(R.layout.dialog, null)
+            builder.setView(v1)
+
+            // p0에 해당 AlertDialog가 들어온다. findViewById를 통해 view를 가져와서 사용
+            val listener = DialogInterface.OnClickListener { p0, p1 ->
+                val alert = p0 as AlertDialog
+                val edit1: EditText? = alert.findViewById<EditText>(R.id.editText)
+                val edit2: EditText? = alert.findViewById<EditText>(R.id.editText2)
+
+                binding.tvTitle.text = "이름 : ${edit1?.text}"
+                binding.tvTitle.append(" : 나이 : ${edit2?.text}")
+            }
+
+            builder.setPositiveButton("확인", listener)
+            builder.setNegativeButton("취소", null)
+            */
+
+
             /** 프로필 사진, 사용자 이름 추가 기능 위치
              ** 다이얼로그 생성하고 사용하기 커스텀 or 기본*/
         }
