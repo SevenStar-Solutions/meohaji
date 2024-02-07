f6269a688b4bef1482531bc984db07b9bec1d397
dongchul <wade316>
2024-02-07 20:58:34 +0900

    feat: 검색어로 검색한 데이터 RecyclerView에 띄우기 구현

--- app/src/main/java/com/example/meohaji/NetworkInterface.kt
+++ app/src/main/java/com/example/meohaji/NetworkInterface.kt
@@ -1,6 +1,7 @@
 package com.example.meohaji
 
 import retrofit2.Call
+import com.example.meohaji.Search.SearchResult
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -31,4 +32,16 @@ interface NetworkInterface {
         @Query("part") part: String?,
         @Query("id") channelId: String?
     ) : Call<Channel?>?
+
+    // q 타입 max 파트 오더 Regioncode
+    @GET("search")
+    suspend fun searchByQueryList(
+        @Query("key") key: String?,
+        @Query("part") part: String?,
+        @Query("maxResults") max: Int?,
+        @Query("order") order: String?,
+        @Query("q") query: String?,
+        @Query("regionCode") code: String?,
+        @Query("type") type: String?,
+    ) : SearchResult
 }
